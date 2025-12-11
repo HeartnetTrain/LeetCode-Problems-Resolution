9df5d279ba1ab1be9bca1078fd6732a1636b07d6
Wael MESSAOUD <messaoudwael2@gmail.com>
2025-12-11 21:52:49 +0100

    Optimize Two Sum problem resolution

--- easy/Two Sum/twoSum.js
+++ easy/Two Sum/twoSum.js
@@ -1,18 +1,18 @@
-var twoSum = function(nums, target) {
+function twoSum(nums, target) {
   var map = new Map();
   for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [i, map.get(complement)]
+    }
     map.set(nums[i], i);
   }
-  for (let i = 0; i < nums.length; i++) {
-    let remaining = target - nums[i];
-    if (map.get(remaining) && map.get(remaining) != i) {
-      return [i, map.get(remaining)];
-    }
-  }
 };
-console.log(twoSum([3, 3], 6));
 
-/* nums = [2,4,3] and target = 6 */
-/* nums = [3,3] and target = 6 */
-/* nums = [1,5,7] and target = 8 */
-/* nums = [8, 15, 0,15,60] and target = 30 */
+/* 
+nums = [2,4,3] and target = 6 => output is [0,1]
+nums = [3,3] and target = 6 => output is [0,1]
+nums = [1,5,7] and target = 8 => output is [0,2]
+nums = [8, 15, 0,15,60] and target = 30 => output is [1,3]
+*/
+
